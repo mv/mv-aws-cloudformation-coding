78b46751ce681b9cb0aeb899f24f806a4aff0a97
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2018-09-07 00:23:28 -0300

    Using parameter list.

--- update-stack.sh
+++ update-stack.sh
@@ -6,8 +6,10 @@ name="${file%.*}"
 name="${name%.*}"
 
 [ "${file}" ] || exit 0
+[ "${2}"    ] && param="--parameters file://${2}"
 
 aws cloudformation update-stack \
   --stack-name ${name}  \
-  --template-body file://"${file}"
+  --template-body file://"${file}" \
+  ${param}
 
